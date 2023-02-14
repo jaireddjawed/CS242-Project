655849a2161cc58a77bf412026ab7496c72d0107
Kunal Mamgain <mamgainkunal@gmail.com>
2023-02-13 16:06:42 -0800

    Updated Phase 1 code for getting values for the graph.

--- pylucene.py
+++ pylucene.py
@@ -1,6 +1,7 @@
 import logging, sys
 logging.disable(sys.maxsize)
 
+import time
 import lucene
 import os
 import json
@@ -15,12 +16,18 @@ from org.apache.lucene.index import FieldInfo, IndexWriter, IndexWriterConfig, I
 from org.apache.lucene.search import IndexSearcher, BoostQuery, Query
 from org.apache.lucene.search.similarities import BM25Similarity
 
-#Update path of json file accordingly
-file_path="/home/cs242/242_New/CS242-Project/posts/bestoflegaladvice.json"
+file_name = sys.argv[1]
+query_word = sys.argv[2]
+
+file_path="./posts/" + file_name + ".json"
 f = open(file_path,'r+')
 sample_doc = json.load(f)
+global count 
 
 def create_index(dir):
+    count = 0
+    #start = time.time()
+
     if not os.path.exists(dir):
         os.mkdir(dir)
     store = SimpleFSDirectory(Paths.get(dir))
@@ -37,9 +44,10 @@ def create_index(dir):
     contextType.setStored(True)
     contextType.setTokenized(True)
     contextType.setIndexOptions(IndexOptions.DOCS_AND_FREQS_AND_POSITIONS)
-    
+
 #Title, author, text, and all post comments are indexed.
     for sample in sample_doc:
+        #count = count + 1
         title = sample['title']
         author = sample['author']
         text = sample['text']
@@ -47,21 +55,28 @@ def create_index(dir):
         all_comments = []
         for comment in comments:
             all_comments.append(comment["text"])
-        
+
         doc = Document()
         doc.add(Field('Title', str(title),metaType))
         doc.add(Field('Author', str(author), contextType))
         doc.add(Field('Text', str(text), contextType))
         doc.add(Field('Comments', str(all_comments), contextType))
-
         writer.addDocument(doc)
+
+        #Condition to calculate the time required for processing in Lucene
+        #if( count == 100 or  count == 200 or count == 300 or count == 400 or count == 500 or count == 600 or count == 700 or count == 800 or count == 900 ):
+            print("Time taken at count ", count, " is ", time.time()  - start," seconds")
+   
     writer.close()
+   
+    #end = time.time()
+    
+    #print("Time taken to create the index: ", end - start," seconds")
 
 def retrieve(storedir, query):
     searchDir = NIOFSDirectory(Paths.get(storedir))
     searcher = IndexSearcher(DirectoryReader.open(searchDir))
-    
-    #parser = QueryParser('Context', StandardAnalyzer())
+
     parser = MultiFieldQueryParser(['Title','Author','Text','Comments'],StandardAnalyzer())
     parser.setDefaultOperator(QueryParserBase.OR_OPERATOR)
     parsed_query = MultiFieldQueryParser.parse(parser,query)
@@ -76,11 +91,11 @@ def retrieve(storedir, query):
             "Text": doc.get("Text"),
             "Author": doc.get("Author"),
             "Comments": doc.get("Comments")
-        })
-#To get the urls for the retrieved documents
+            })
+        #To get the urls of the retrieved documents
     for i, docs in enumerate(topkdocs):
         topkdocs[i]["url"] = sample_doc[i]["url"]
-
+        #Only relevant fields are printed
     for docs in topkdocs:
         print("Score: ", docs["score"])
         print("Title: ", docs["Title"])
@@ -90,10 +105,8 @@ def retrieve(storedir, query):
         #print("Comments: ", docs["Comments"])
         print("")
 
-
 lucene.initVM(vmargs=['-Djava.awt.headless=true'])
 create_index('sample_lucene_index/')
-#Our sample query runs on keyword `driver` for the subreddit r/bestoflegaladvice
-retrieve('sample_lucene_index/', 'plowing')
-
+#Our sample query runs on query word and subreddit name passed as a argument 
+retrieve('sample_lucene_index/', query_word)
 
