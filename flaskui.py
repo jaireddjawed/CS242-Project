6d98bacbdaa127a1539f8e9a028e9799b7eda893
marirazak <mariamrazak01@gmail.com>
2023-03-17 01:47:24 -0700

    working on ui

--- flaskui.py
+++ flaskui.py
@@ -1,15 +1,17 @@
-#import flask
+import flask
 from flask import Flask, render_template, request
 from pylucene import retrieve
 from search_engine import retrieve_documents
+from flask_bootstrap import Bootstrap
 
 #create_index('sample_lucene_index/')
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('home_b.html')
 
 @app.route('/search', methods=['GET', 'POST'])
 def search():
@@ -22,7 +24,7 @@ def search():
             results = retrieve_documents(query)
         return render_template('results.html', query=query, results=results)
     else:
-        return render_template('home.html')
+        return render_template('home_b.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8888, debug=True)
